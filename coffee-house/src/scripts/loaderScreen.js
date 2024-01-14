f31800df92efa8efbea445dd576056bdcfb34c9a
Alexey Melnikov <cancem343@gmail.com>
2024-01-14 10:47:48 +0300

    Updates

--- coffee-house/src/scripts/loaderScreen.js
+++
@@ -1,6 +0,0 @@
-export function showLoader() {
-  document.addEventListener("DOMContentLoaded", () => {
-    const loader = document.querySelector(".loader");
-    setTimeout(() => loader.style.display = 'none', 300);
-  });
-};
