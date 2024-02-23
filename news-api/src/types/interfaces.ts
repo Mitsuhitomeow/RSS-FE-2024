553806efe537302a768082615790fe5e9735dd16
Alexey Melnikov <cancem343@gmail.com>
2024-02-24 00:35:05 +0300

    feat: change file extensions to .ts, write interfaces, add typing to news.ts

---
+++ news-api/src/types/interfaces.ts
@@ -0,0 +1,60 @@
+export interface ArrSource {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+export interface SourceTypes extends ArrSource {
+    status: string;
+    source: ArrSource[];
+}
+
+export interface Article {
+    source: {
+        id: string | null;
+        name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+export interface NewsArticle extends Article {
+    status: string;
+    totalResults: number;
+    articles: Article[];
+}
+
+export interface ArrHeadlines {
+    source: {
+        id: string | null;
+        name: string;
+    };
+    author: string | null;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+export interface Headlines extends ArrHeadlines {
+    status: string;
+    totalResults: number;
+    articles: ArrHeadlines[];
+}
+
+export interface Errors {
+    status: string;
+    code: string;
+    message: string;
+}
