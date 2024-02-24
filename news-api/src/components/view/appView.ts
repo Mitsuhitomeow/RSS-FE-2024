2e5ceb90e2bd314629b16a24424db0d0603e0355
Alexey Melnikov <cancem343@gmail.com>
2024-02-24 21:10:13 +0300

    feat: change file extensions to .ts, add typing to appView.ts

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,18 +1,22 @@
+import { NewsArticle, SourceTypes } from '../../types/interfaces';
 import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: NewsArticle) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: SourceTypes) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
