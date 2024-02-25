32b97e773a9082b0ffe921a606ab2b3ec17928ac
Alexey Melnikov <cancem343@gmail.com>
2024-02-25 19:02:37 +0300

    feat: change file extensions to .ts, add typing to appLoader.ts

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,6 +2,11 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
+
+        if (!process.env.API_URL || !process.env.API_KEY) {
+            throw new Error('environment variable is not defined');
+        }
+
         super(process.env.API_URL, {
             apiKey: process.env.API_KEY,
         });
