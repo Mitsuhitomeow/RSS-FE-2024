31df3f1c87ff3591e662159cfdc5ecc6b1c70be5
Alexey Melnikov <cancem343@gmail.com>
2024-02-25 19:35:38 +0300

    feat: change file extensions to .ts, add typing to controller.ts

--- news-api/src/components/controller/controller.ts
+++ news-api/src/components/controller/controller.ts
@@ -1,7 +1,8 @@
+import { LoadCallBack } from '../../types/types';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources<T>(callback: LoadCallBack<T>) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,14 +11,18 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
+    getNews<T>(e: Event, callback: LoadCallBack<T>) {
         let target = e.target;
         const newsContainer = e.currentTarget;
 
-        while (target !== newsContainer) {
+        while (
+            target !== newsContainer &&
+            target instanceof HTMLElement &&
+            newsContainer instanceof HTMLElement
+        ) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                if (newsContainer.getAttribute('data-source') !== sourceId && sourceId !== null) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
                         {
