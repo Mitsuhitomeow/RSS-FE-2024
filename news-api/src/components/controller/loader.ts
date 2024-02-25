99eb2185281d5836bbcc60c1d4969f47d406c5cb
Alexey Melnikov <cancem343@gmail.com>
2024-02-25 17:48:31 +0300

    feat: change file extensions to .ts, add typing to loader.ts

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,11 +1,16 @@
+import { LoadCallBack, RespEndpoint, RespOption } from '../../types/types';
+
 class Loader {
-    constructor(baseLink, options) {
+    protected baseLink: string;
+    protected options: { apiKey: string };
+
+    constructor(baseLink: string, options: { apiKey: string }) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint, options = {} }: { endpoint: RespEndpoint; options?: RespOption },
         callback = () => {
             console.error('No callback for GET response');
         }
@@ -13,7 +18,7 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,18 +28,18 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(options: RespOption, endpoint: RespEndpoint) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
+            url += `${key}=${urlOptions[key as keyof RespOption]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load<T>(method: string, endpoint: RespEndpoint, callback: LoadCallBack<T>, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
