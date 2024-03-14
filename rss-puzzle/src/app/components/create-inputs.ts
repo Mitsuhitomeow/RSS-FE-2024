365878960d0d92dfc1fa0301783a1eda6173b495
Alexey Melnikov <cancem343@gmail.com>
2024-03-14 20:43:18 +0300

    refactor: update a creator inputs

--- rss-puzzle/src/app/components/create-inputs.ts
+++ rss-puzzle/src/app/components/create-inputs.ts
@@ -2,14 +2,16 @@ import type { InputProps } from '../interfaces/form.interface';
 import Component from './create-components';
 
 export default class InputComponent extends Component {
-  protected placeholder: string;
+  protected placeholder;
 
   constructor({ placeholder, className, types }: InputProps) {
     super({ tag: 'input', className });
-    this.placeholder = placeholder;
 
+    this.placeholder = placeholder;
     this.node = this.getNode();
+
     const inputType = this.node as HTMLInputElement;
+    inputType.placeholder = placeholder;
 
     if (types) {
       inputType.type = types;
