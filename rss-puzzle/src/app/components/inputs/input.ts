e88ab16d3fc9bb264da1891cfceec4c794829336
Alexey Melnikov <cancem343@gmail.com>
2024-03-14 20:44:58 +0300

    feat: add inputs to the form

---
+++ rss-puzzle/src/app/components/inputs/input.ts
@@ -0,0 +1,12 @@
+import InputComponent from '../create-inputs';
+import styles from './input.module.scss';
+
+export const InputFirstName = new InputComponent({
+  placeholder: 'First Name',
+  className: `input ${styles.input__name}`,
+});
+
+export const InputLastName = new InputComponent({
+  placeholder: 'Last Name',
+  className: `input ${styles.input__name}`,
+});
