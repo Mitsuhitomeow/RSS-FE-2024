cb46b9595d4c7610b5e1992d032d640cf91a53f6
Alexey Melnikov <cancem343@gmail.com>
2024-03-15 21:51:36 +0300

    refactor: update eslint

--- rss-puzzle/.eslintrc.cjs
+++ rss-puzzle/.eslintrc.cjs
@@ -43,6 +43,12 @@ module.exports = {
             endOfLine: 'lf',
           },
         ],
+        'object-curly-newline': [
+          'error',
+          {
+            consistent: true
+          }
+        ],
         '@typescript-eslint/consistent-type-imports': 'error',
       },
     },
