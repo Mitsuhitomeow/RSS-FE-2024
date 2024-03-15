391aad06190cf5e43ed6e7501656081f4957ae30
Alexey Melnikov <cancem343@gmail.com>
2024-03-15 17:36:56 +0300

    refactor: update app.ts

--- rss-puzzle/src/app/app.ts
+++ rss-puzzle/src/app/app.ts
@@ -1,17 +1,13 @@
 import AuthPage from './pages/authorization';
 
 export default class App {
-  private element;
-
   private authPage;
 
   constructor() {
     this.authPage = AuthPage;
-    this.element = document.createElement('h1');
   }
 
   public start(): void {
-    this.element.textContent = 'Hello World!!';
-    document.body.append(this.element, this.authPage.getNode());
+    document.body.append(this.authPage.getNode());
   }
 }
